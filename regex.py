dd2d0290b846e9722187731d472c7eff832fe2aa
meghaagr13 <agarwalmegha1308@gmail.com>
2018-01-26 01:45:16 +0530

    tokens modified

--- regex.py
+++ regex.py
@@ -1,8 +1,8 @@
 from tokens import *
 
-t_INT = r'[+-]?([0-9])+'
-t_FLOAT = r'([+-])?[0-9]+\.([0-9]+([Ee]?[+-][0-9]+)?)?'
-t_BOOL = r'(True | False)'
+#t_INT = r'[+-]?([0-9])+'
+#t_FLOAT = r'([+-])?[0-9]+\.([0-9]+([Ee]?[+-][0-9]+)?)?'
+t_BOOL = r'(true | false)'
 t_LEFTARROW = r'<-'
 t_GT = r'>'
 t_GE = r'>='
@@ -25,21 +25,48 @@ t_AND = r'&&'
 t_OR = r'\|\|'
 t_NOT = r'!'
 t_XOR = r'\^'
+t_AND_BIT = '&'
+t_OR_BIT = '\|'
 t_LSHIFT = r'<<'
 t_RSHIFT = r'>>'
-t_rrshift = r'>>>'
-t_EQUALASS = r'='
-t_ADDASS = r'\+='
-t_SUBASS = r'-='
-t_MULASS = r'\*='
-t_MODASS = r'%='
-t_DIVASS = r'/='
-t_LSHIFTASS = r'<<='
-t_RSHIFTASS = r'>>='
-t_ANDASS = r'&='
-t_ORASS = r'\|='
-t_XORASS = r'\^='
+t_RRSHIFT = r'>>>'
+t_EQUALASGN = r'='
+t_ADDASGN = r'\+='
+t_SUBASGN = r'-='
+t_MULASGN = r'\*='
+t_MODASGN = r'%='
+t_DIVASGN = r'/='
+t_LSHIFTASGN = r'<<='
+t_RSHIFTASGN = r'>>='
+t_ANDASGN = r'&='
+t_ORASGN = r'\|='
+t_XORASGN = r'\^='
 t_COLON = r':'
 t_SEMICOLON = r';'
 t_DOT = r'\.'
 t_COMMA = r'\,'
+t_UNDER = r'_'
+
+#Litereals
+
+def t_INT(t):
+    r'((\d+)|(((0x)|(0X))[0-9A-Fa-f]+))[lL]?'
+
+def t_FLOAT(t):
+    r'((\d+)?(\.)(\d+)([Ee][+-]?(\d+))?([FfDd])?)| ((\d)+([Ee][+-]?(\d+))?([FfDd]))'
+
+    if t.value[-1]=='F' or t.value[-1]=='f' or t.value[-1]=='D' or t.value[-1]=='d' :
+        t.value = t.value[:-1]
+        t.value = float(t.value)
+        return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
+
+
+
+
+
+
