2d0c1019cca50d8b9c8a095860cb2c67a80b2c0e
jaismin <jaismin07@gmail.com>
2018-01-26 05:33:01 +0530

    formatted output

--- lexer.py
+++ lexer.py
@@ -27,8 +27,21 @@ lexer.input('''abc HelloWorld {
   }
 ''')
 
+tokens_d = {}
+
 while True:
 	token = lexer.token()
 	if not token:
 		break
-	print(token)
+	# print(token)
+	if not token.value in tokens_d:
+		tokens_d[token.value] = token.type, 1
+	else:
+		tokens_d[token.value] = token.type, tokens_d[token.value][1]+1
+
+print '%-20s%-20s%-20s' % ("Token", "Occurances", "Lexemes")
+print "____________________________________________________"
+for value in tokens_d:
+	print '%-20s%-20s%-20s' % (tokens_d[value][0], tokens_d[value][1], value)
+
+
