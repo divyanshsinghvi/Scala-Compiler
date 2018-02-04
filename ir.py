d0b6ef72c0afe1dd2f7b1893e3aefdad9e0b1e79
jaismin <jaismin07@gmail.com>
2018-02-04 16:56:15 +0530

    reading the csv to create the array

--- ir.py
+++ ir.py
@@ -1,86 +1,96 @@
 import csv
-from enum import Enum
-from goto import goto, label
 
+PLUS ='+'
+MINUS='-'
+TIMES='*'
+DIV='/'
+AND='&&'
+OR ='||'
+LT='<'
+LE='<='
+EQ='=='
+GE='>='
+GT='>'
+NE='!='
+NOT='!'
+op = {}
+op['binop']=[PLUS, MINUS, TIMES, DIV, AND, OR, LT, LE, EQ, GE, GT, NE]
+op['unop']=[NOT]
+op['assign']= ['=']
+op['goto']=['goto']
+op['if']=['if']
+op['param']=['param']
+op['call']=['call']
+op['fcall']=['fcall']
+op['return']=['return']
+op['freturn']=['freturn']
+op['ldar']=['ldar']
+op['star']=['star']
+op['label']=['label']
 class ir:
     
     out=None
     in1=None
     in2=None
-    PLUS ='+'
-    MINUS='-'
-    TIMES='*'
-    DIV='/'
-    AND='&&'
-    OR ='||'
-    LT='<'
-    LE='<='
-    EQ='=='
-    GE='>='
-    GT='>'
-    NE='!='
-    NOT='!'
-    op = {}
-    op['binop']=[PLUS, MINUS, TIMES, DIV, AND, OR, LT, LE, EQ, GE, GT, NE]
-    op['unop']=[NOT]
-    op['assign']= '='
-    op['goto']='goto'
-    op['if']='if'
-    op['param']='param'
-    op['call']='call'
-    op['fcall']='fcall'
-    op['return']='return'
-    op['freturn']='freturn'
-    op['ldar']='ldar'
-    op['star']='star'
 
-    def __init__(self, op, out, in1, in2):
-        self.oper = oper
-        self.out = out #x
-        self.in1 = in1 #y
-        self.in2 = in2 #z
-    
-    def Insert(self, rowList):
-        operator = rowList[0]
-        for key, val in Dict.iteritems():
-            
-        
+    def __init__(self, rowList):
+        self.oper = rowList[0]
+        #self.out = out  #x
+        #self.in1 = in1 #y
+        #self.in2 = in2 #z
+        #print(rowList[0])   
+    #def MakeEntry(rowList):
+    #    row = ir(rowList[0],null, null, null)
+        for key, val in op.iteritems():
+            if rowList[0] in val:
+                operator = key;
+                break
+        #print(operator)        
         if operator == 'binop':
-            out = rowList[1]
-            in1 = rowList[2] 
-            in2 = rowList[3]
+            self.out = rowList[1]
+            self.in1 = rowList[2] 
+            self.in2 = rowList[3]
         elif operator == 'unop':
-            out = rowList[1]
-            in1 = rowList[2]
-            in2 = 0
+            self.out = rowList[1]
+            self.in1 = rowList[2]
+            self.in2 = 0
         elif operator == 'assign':
-            out = rowList[1]
-            in1 = rowList[2]
+            self.out = rowList[1]
+            self.in1 = rowList[2]
         elif operator == 'goto':
-            out = rowList[1]
+            self.out = rowList[1]
         elif operator == 'if':
-            out = rowList[1]
-            in1 = rowList[2]
+            self.out = rowList[1]
+            self.in1 = rowList[2]
         elif operator == 'param':
-            in1 =rowList[1]
+            self.in1 =rowList[1]
         elif operator == 'fcall':
-            out = rowList[1]
-            in1 = rowList[2]
-            in2 = rowList[3]
+            self.out = rowList[1]
+            self.in1 = rowList[2]
+            self.in2 = rowList[3]
         elif operator == 'freturn':
-            in1 = rowList[1]
+            self.in1 = rowList[1]
         elif operator == 'ldar':
-            out = rowList[1]
-            in1 = rowList[2]
-            in2 = rowList[3]
+            self.out = rowList[1]
+            self.in1 = rowList[2]
+            self.in2 = rowList[3]
         elif operator == 'star':
-            out = rowList[1]
-            in1 = rowList[2]
-            in2 = rowList[3]
+            self.out = rowList[1]
+            self.in1 = rowList[2]
+            self.in2 = rowList[3]
+        elif operator == 'call':
+            self.in1 = rowList[1]
+            self.in2 = rowList[2]
+        elif operator =='label':
+            self.out = rowList[1]
 
+array = []
 
 with open('3ac.csv', 'r') as f:
     reader = csv.reader(f, delimiter=',', quotechar='"', doublequote=True, quoting=csv.QUOTE_MINIMAL)
     for row in reader:
-        for element in row:
+        #print(ir(row))
+        array.append(ir(row))
 
+print [label.oper for label in array]
+        
