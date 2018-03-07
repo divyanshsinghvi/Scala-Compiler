910a4d9ba0ebb07b760374ef6a1357a2824c0a16
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-07 14:00:27 +0530

    sample file calc added for parser table

--- src/parser.py
+++ src/parser.py
@@ -1,8 +1,14 @@
+#import lex
 import yacc
-import tokens
+from tokens import tokens
 
 
-
+def p_ADD(p):
+    ''' ADD : INT OP_ADD INT
+    '''
+    p[0] = p[1] + p[3]
+    print p[0]
 
 parser = yacc.yacc()
+parser.parse('2+3')
 
