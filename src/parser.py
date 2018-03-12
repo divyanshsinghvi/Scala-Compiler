cd278399d36c5f7000a00a99cca6c99552cfd836
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-12 19:50:42 +0530

    print statement removed

--- src/parser.py
+++ src/parser.py
@@ -5,8 +5,9 @@ import lexer
 tokens = lexer.tokens
 
 def printp(p):
-    for i in range(0,len(p)):
-        print (p.slice)[i],
+    print (p.slice)
+   # for i in range(0,len(p)):
+   #     print (p.slice)[i],
 
     print "\n"
 
@@ -222,12 +223,10 @@ def p_val_dcl_0(p):
                     |   COMMA val_dcl'''
     printp(p)
 
-    printp(p)
 def p_var_dcl(p):
     '''var_dcl  :   id COLON type var_dcl_0'''
     printp(p)
 
-    printp(p)
 def p_var_dcl_0(p):
     '''var_dcl_0    :   epsilon
                     |   COMMA var_dcl'''
@@ -238,7 +237,6 @@ def p_fun_dcl(p):
                 |   fun_sig'''
     printp(p)
 
-    printp(p)
 #def fun_dcl_1(p):
 #    '''fun_dcl_0    :   COLON type fun_dcl_0
 #                    |   epsilon'''
