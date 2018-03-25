180db6a540ae7ce4b892fa6a1d3519f5a73f6ce6
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-25 13:43:46 +0530

    dangling if else handled

--- parserconvert/dangling_if_else.scala
+++ parserconvert/dangling_if_else.scala
@@ -1,6 +1,7 @@
 object Demo {
     def main(args: Array[String])={
-    var i: Int = 0, a: Array[Int] ={c,b};
+    var i: Int = 0;
+    var  a: Array[Int] ={c,b,d};
     if (i<=3){
         a[i]=a[i]+1;
     };
