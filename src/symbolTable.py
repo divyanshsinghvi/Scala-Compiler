513cc59a7bcece0f6298f62d69ce7961a87d8afc
jaismin <jaismin07@gmail.com>
2018-03-28 19:08:21 +0530

    env in symbol table started

--- src/symbolTable.py
+++ src/symbolTable.py
@@ -1,48 +1,42 @@
-##########################SymbolTable############################################
-#Dictionary of Dictionary to store nextUse and livelniess information about all variab
-
-class SymbolTable:
-    idNo = 0
-    def __init__(self):
-        self.var = {}           #dictionary of all variables
-    
-    def setVar(self,name,attribute):
-        if name not in self.var:
-            self.var[name]=attribute
+class Env:
+    tableNo = 0
+    def __inti__(self, prev=None):
+        self.symbolTable = HashTable()
+        self.functionTable = HashTable()
+        self.objectTable = HashTable()
+        self.name = "Table" + str(env.tableNo)
+        self.children = []
+        self.width = 0
+        self.prev = prev
+        self.code = []
+        self.startChildBlock = None
+        self.endChildBlock = None
+        self.funcName = None
+        self.objName = None
+        self.offset = 0
+        if prev == None:
+            self.level = 0
+            self.parentName = ""
         else:
-            for key in attribute:
-                self.var[name][key]=attribute[key]
+            self.level = prev.level + 1
+            self.parentName = prev.name
+        Env.tableNo += 1
+        if(prev != None):
+            prev.children.append(self)
+        self.totalSize = 0
 
-    def getVar(self,name,value):
-        if name not in self.var:
-            return None
-        if value not in self.var[name]:
-            return None
-        return self.var[name][value]
+        def gprev(self):
+            return self.prev
 
-    def print_symboltable(self):
-        
-        print("----------------------------------------")
-        if not self.var:
-            return 
-        for name in self.var:
-            print(name)
-            #print("Attribute for variable :")
-            for key in self.var[name]:
-                print(key+"="+str(self.var[name][key]))
-        print("----------------------------------------")
-    
+        def goto(self, data):
+            for child in range(0, len(slef.children)):
+                if(self.children[child].name == data):
+                    return self.children[child]
 
-#Example to check symbol table 
-if __name__ == '__main__':
-    s=SymbolTable()
-    s.set('a',{'name':'zara','type':'int','age':'7'})
-    s.print_symboltable()
-    s.set('b',{'age':7,'name':'megha'})
-    s.print_symboltable()
-    print(s.get('a','name'))
-    print(s.get('a','height'))
-    s.set('b',{'height':'11'})
-    s.print_symboltable()
+        def subtree(self):
+            print "NAME: ", self.name, "LEVEL: ", self.level
+            self.symbolTable.printTable()
+            for child in range(0, len(self.children)):
+                self.child.subtree()
 
-
+        def addEntry(self, name, list_attributes, updateField = "symbol"):
