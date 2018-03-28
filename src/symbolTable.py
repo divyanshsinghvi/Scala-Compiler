20e21252316c6810874481b53a68f5c97f12daed
meghaagr13 <agarwalmegha1308@gmail.com>
2018-03-28 18:45:03 +0530

    symbolTable new

--- src/symbolTable.py
+++ src/symbolTable.py
@@ -1,48 +1,18 @@
-##########################SymbolTable############################################
-#Dictionary of Dictionary to store nextUse and livelniess information about all variab
+from sets import Set
 
-class SymbolTable:
-    idNo = 0
+class HashTable:
+
     def __init__(self):
-        self.var = {}           #dictionary of all variables
+        self.table={}
     
-    def setVar(self,name,attribute):
-        if name not in self.var:
-            self.var[name]=attribute
-        else:
-            for key in attribute:
-                self.var[name][key]=attribute[key]
+    def addEntry(self,name,listAttributes)
 
-    def getVar(self,name,value):
-        if name not in self.var:
-            return None
-        if value not in self.var[name]:
-            return None
-        return self.var[name][value]
 
-    def print_symboltable(self):
+
+class Env:
+    static_var = 1
+    def __init__(self, prev=None):
+        self.symbolTable = HashTable()  #Creates a new symbol table for a new scope
+        self.functionTable = HashTable()  #Creates a new hash table for a new scope
+        self.objectTable = HashTable()  #Creates a new hash table for a new scope
         
-        print("----------------------------------------")
-        if not self.var:
-            return 
-        for name in self.var:
-            print(name)
-            #print("Attribute for variable :")
-            for key in self.var[name]:
-                print(key+"="+str(self.var[name][key]))
-        print("----------------------------------------")
-    
-
-#Example to check symbol table 
-if __name__ == '__main__':
-    s=SymbolTable()
-    s.set('a',{'name':'zara','type':'int','age':'7'})
-    s.print_symboltable()
-    s.set('b',{'age':7,'name':'megha'})
-    s.print_symboltable()
-    print(s.get('a','name'))
-    print(s.get('a','height'))
-    s.set('b',{'height':'11'})
-    s.print_symboltable()
-
-
