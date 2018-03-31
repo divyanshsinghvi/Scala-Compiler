72d13c4b53e5747489f2fcd3d9a62251380be0ef
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-03-31 10:53:22 +0530

    symboltableold rename

---
+++ src/symbolTableOld.py
@@ -0,0 +1,48 @@
+##########################SymbolTable############################################
+#Dictionary of Dictionary to store nextUse and livelniess information about all variab
+
+class SymbolTable:
+    idNo = 0
+    def __init__(self):
+        self.var = {}           #dictionary of all variables
+    
+    def setVar(self,name,attribute):
+        if name not in self.var:
+            self.var[name]=attribute
+        else:
+            for key in attribute:
+                self.var[name][key]=attribute[key]
+
+    def getVar(self,name,value):
+        if name not in self.var:
+            return None
+        if value not in self.var[name]:
+            return None
+        return self.var[name][value]
+
+    def print_symboltable(self):
+        
+        print("----------------------------------------")
+        if not self.var:
+            return 
+        for name in self.var:
+            print(name)
+            #print("Attribute for variable :")
+            for key in self.var[name]:
+                print(key+"="+str(self.var[name][key]))
+        print("----------------------------------------")
+    
+
+#Example to check symbol table 
+if __name__ == '__main__':
+    s=SymbolTable()
+    s.set('a',{'name':'zara','type':'int','age':'7'})
+    s.print_symboltable()
+    s.set('b',{'age':7,'name':'megha'})
+    s.print_symboltable()
+    print(s.get('a','name'))
+    print(s.get('a','height'))
+    s.set('b',{'height':'11'})
+    s.print_symboltable()
+
+
