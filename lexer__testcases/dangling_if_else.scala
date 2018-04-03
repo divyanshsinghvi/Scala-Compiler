2a574d050c1b218da5d00a464c0c3941f695fabf
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-04-03 22:53:21 +0530

    if else working code checked

--- lexer__testcases/dangling_if_else.scala
+++ lexer__testcases/dangling_if_else.scala
@@ -2,16 +2,21 @@ object Demo {
     def main(args: Array[String](2))={
     var i: Int = 0;
     var b: Int = 3;
+    var c: Int = 2;
+    var d: Int = 1;
+    var e: Int = 3;
+    var f: Int = 2;
+
     //var a: Array[Int](2) = {1,2};
-    if (i<=3){
-	b = b + i;
+    if (i<=e){
+	b = b + f;
         //a[i]=a[i]+1;
     };
-    if (i>=2){
-	b = b + 2*i;
+    if (i>=f){
+	b = b + c*f;
         //a[i]=a[i]-1;
     } else {
-        b = 1;
+        b = d;
     };
   };
 };
