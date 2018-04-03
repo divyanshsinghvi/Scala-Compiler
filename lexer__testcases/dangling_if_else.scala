8d6c816c63b4bd9fec790f743ca3b73e1e726fa6
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-04-03 22:31:36 +0530

    some

--- lexer__testcases/dangling_if_else.scala
+++ lexer__testcases/dangling_if_else.scala
@@ -1,14 +1,17 @@
 object Demo {
     def main(args: Array[String](2))={
     var i: Int = 0;
-   // var a: Array[Int](2) = {1,2};
+    var b: Int = 3;
+    //var a: Array[Int](2) = {1,2};
     if (i<=3){
-        a[i]=a[i]+1;
+	b = b + i;
+        //a[i]=a[i]+1;
     };
     if (i>=2){
-        a[i]=a[i]-1;
+	b = b + 2*i;
+        //a[i]=a[i]-1;
     } else {
-        a[i] = 1;
+        b = 1;
     };
   };
 };
--- src/parsermain.py
+++ src/parsermain.py
@@ -43,7 +43,7 @@ precedence = (
 def evalArray(temp):
     if temp['type'] == 'Array':
         t1 = ST.getTemp()
-        print "I am here"
+        #print "I am here"
         emit(op='ldar',out=t1,in1=temp['place'],in2=temp['index'])
         r = {
                 'place' : t1,
@@ -331,7 +331,7 @@ def p_fun_sig(p):           # function is named id@no.of args
     #            "args":arg,
     #            "return":True
     #        }
-    #    emit("flabel",p[1])
+    emit("flabel",p[1])
     #printp(p)
 
 def p_param_clause(p):
@@ -908,7 +908,7 @@ def p_assign(p):
         p[3]=evalArray(p[3])
         t1 = ST.getTemp()
         t2 = evalArray(p[1])
-        print p[2][0]
+        #print p[2][0]
         emit(p[2][0],t1,t2['place'],p[3]['place'])
         if p[1]['type'] == 'Array':
             emit('star',p[1]['place'],p[1]['index'],t1)
