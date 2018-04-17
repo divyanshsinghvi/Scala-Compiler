649eab0c3345d2617b91463d058dc7d1256e61aa
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-04-18 01:58:24 +0530

    xor and or bit implemented

---
+++ lexer__testcases/array.scala
@@ -0,0 +1,12 @@
+object Demo {
+    def main(var d:Int)={
+    //var m:Int=3;
+    var a: Array[Int](2,1) = {200,300};
+    println(a[1,0]);
+	var m:Int = a[1,0]^1;
+        var b:Int = a[1,0];
+	println(m);
+	println(b);
+    return 0;
+};
+};
