afff6cc01ea78b486ea39ad115edd29b3733a181
meghaagr13 <agarwalmegha1308@gmail.com>
2018-04-20 15:02:48 +0530

    lhs rhs array ambiguity CHECK

---
+++ final_test/matrix.scala
@@ -0,0 +1,25 @@
+class demo(){
+def main()={
+    var i: Int =1;
+    var j: Int = 2;
+    var d: Int =2;
+    
+    var m1:Array[Int](2,2)={1,2,3,4};
+    var m2:Array[Int](2,2)={100,200,300,400}; 
+    var m3:Array[Int](2,2)={1,1,1,1}; 
+    
+    for(i=0;i<2;i+=1)
+    {for(j=0;j<2;j+=1)
+      {
+        d = m1[i,j]*m2[i,j];
+        m3[i,j] += d;
+        println(m3[i,j]);
+
+      };
+    };
+    
+    
+    return j;
+};
+};
+//0 2 2 5
